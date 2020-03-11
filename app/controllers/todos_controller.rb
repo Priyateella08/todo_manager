f75b2b8dc4464f6ff4d3541c6aaad772a46b0ec8
likhipriya2001@gmail.com <likhipriya2001@gmail.com>
2020-03-11 17:46:54 +0530

    created users table and display list of users

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,6 +3,7 @@ class TodosController < ApplicationController
 
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    #render plain: User.order(:name).map { |user| user.to_pleasant_string }.join("\n")
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  #get "users", to: "users#index"
   # get "todos", to: "todos#index"
   # post "todos", to: "todos#create"
   # get "todos/:id", to: "todos#show"
-  resources :todos
+  # resources :todos
+  resources :users
 end
