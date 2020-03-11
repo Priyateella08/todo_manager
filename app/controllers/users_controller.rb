4887cb113df92788f4235cd3d8deafddb7fbf475
likhipriya2001@gmail.com <likhipriya2001@gmail.com>
2020-03-11 18:05:05 +0530

    create new entry for user using post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: User.all.map { |u| u.to_s }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_s
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "Hey your new user is created with the id #{new_user.id}"
+    render plain: response_text
   end
 end
