6d8c68374829206c10d175a77f0e9cdf3e652248
likhipriya2001@gmail.com <likhipriya2001@gmail.com>
2020-04-20 14:29:15 +0530

    validating todos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  #  skip_before_action :ensure_user_logged_in
+  skip_before_action :ensure_user_logged_in
 
   def index
     render "users/new"
@@ -16,13 +16,12 @@ class UsersController < ApplicationController
       email: params[:email],
       password: params[:password],
     )
-    redirect_to "/"
-    # if new_user.save
-    #   redirect_to new_sessions_path
-    #   flash[:notice] = "Please Sign in to continue."
-    #  else
-    #   flash[:error] = new_user.errors.full_messages.join(", ")
-    #   redirect_to users_path
-    #  end
+    if new_user.save
+      redirect_to new_sessions_path
+      flash[:notice] = "Please Sign in to continue."
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to users_path
+    end
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,13 @@
 class Todo < ActiveRecord::Base
+  validates :todo_text, presence: true, length: { minimum: 2 }
+  validates :due_date, presence: true
+
   belongs_to :user
+
+  def self.of_user(user)
+    all.where(user_id: user.id)
+  end
+
   def self.overdue
     all.where("due_date < ?", Date.today)
   end
