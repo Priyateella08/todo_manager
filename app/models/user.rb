5a1dde2b4c86db453a40f50b9b9e502654b33aae
likhipriya2001@gmail.com <likhipriya2001@gmail.com>
2020-04-20 10:37:00 +0530

    created a user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
-class User < ActiveRecord::Base
-  def to_s
-    "#{id} #{name} #{email} #{password}"
-  end
+class User < ApplicationRecord
+  # def to_s
+  #"#{id} #{name} #{email} #{password}"
+  # end
 end
